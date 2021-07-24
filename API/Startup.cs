0949044f89921a3c1934e3cf19b6ac687ec56461
kanika <matkanu@gmail.com>
2021-07-24 16:11:05 +0530

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -14,6 +14,8 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Infrastructure.Data;
+using Core.Interfaces;
+
 namespace API
 {
     public class Startup
@@ -29,7 +31,7 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddScoped<IProductRepository, ProductRepository>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
